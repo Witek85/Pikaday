9efee3722dc41652340baac78c130d4a7916085d
Witold Szklarski <w.szklarski@lot.pl>
2020-04-20 08:35:38 +0200

    i18n cancel submit

--- pikaday.js
+++ pikaday.js
@@ -269,6 +269,8 @@
         i18n: {
             previousMonth : 'Previous Month',
             nextMonth     : 'Next Month',
+            clear     : 'Clear',
+            submit     : 'Submit',
             months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
             weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
             weekdaysShort : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
